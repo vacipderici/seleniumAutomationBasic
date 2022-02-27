923ffd49cf77975af410dc1f3bd0208e8669a71b
dericivd@gmail.com <vacipderici@gmail.com>
2022-02-27 16:40:46 +0300

    Initial commit

---
+++ src/test/java/tests/test.java
@@ -0,0 +1,57 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        // Open
+        driver.get("https://demoqa.com/text-box");
+
+        // Maximize browser
+
+        driver.manage().window().maximize();
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        fullName.click();
+        fullName.sendKeys("Vcp");
+
+        WebElement email = driver.findElement(new By.ByCssSelector(".mr-sm-2[id='userEmail']"));
+        email.click();
+        email.sendKeys("mail@mail.com");
+
+
+        WebElement currentAddress = driver.findElement(new By.ByCssSelector(".form-control[placeholder='Current Address']"));
+        currentAddress.click();
+        currentAddress.sendKeys("asdas asdasdas sadasdas sadas ");
+
+        JavascriptExecutor jsx = (JavascriptExecutor)driver;
+        jsx.executeScript("window.scrollBy(0,450)", "");
+
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        permanentAddress.click();
+        permanentAddress.sendKeys("Address Turkey ");
+
+        WebElement submitButton = driver.findElement(new By.ByCssSelector("button.btn"));
+        submitButton.click();
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
